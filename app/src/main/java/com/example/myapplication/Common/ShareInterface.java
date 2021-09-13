511beff71e01fae108c6068b939bd47d81cb1977
JeaPill.Yoon <dbswovlf2009@naver.com>
2021-09-13 16:14:39 +0900

    1. Manifest 추가     전화 수신 허용 및 핸드폰의 상태, 걸려온 전화번호를 사용할 수 있도록 유저퍼미션을 추가하였습니다. 2. MainActivity requestPermission 추가     추가적으로 사용자가 권한수락을 입력해야 사용할 수 있는 권한을 추가하였습니다. 3. CallActivity 생성     전화를 걸거나 받을때 보여줄 Actitivity를 제작하였습니다. 3. CallListener 생성      CallAcitivity에서 유저와의 상호작용에 따라 BroadCastRecevier에서 통화를 끊거나 걸 수 있도록 연결하였습니다 4, CallReceiver 생성     BroadCastReciver로써 phone state중 전화가 왔을 때를 실행됩니다. 5. ShareInterface 생성     CallReceiver가 구현한 CallListener를 CallActivity static타입의 인스턴스로 유지하는 클래스입니다.     해당 방법은 구현은 할 수 있으나 올바른 방향이 아니라고 사료되긴 합니다. 6. layout resource들 추가     CallActivity에서 사용할 layout resourece들을 추가하였습니다.
    * 작업중인 내역
    1. 단말로 수신을 받았을 때, 발신자가 끊어서 phone state가 통화 종료가 되면 CallAcitivity에서 해당 정보를 수신받아야 합니다.
    2.
    * 개선해야할 사항
    1. 통화 수신은 CallActivity로 받을 수 있으나, 발신의 경우 이전 암시적 인텐트를 사용하여 넘어가지지 않고 있음
    2. broadcastRecevier - CallActivity간 리스너 2개를 부착하여 서로간의 상태에 따라 메서드를 실행시키려고 하였으나,
         이 경우엔 bindSevice를 사용하는것이 나아 보입니다.
    3. BroadCastReceiver는 앱이 켜져있는 동안에만 유효하므로,
         ConnectionService와 InCallService API로 통화 기본 앱으로 등록 후에 사용해야 할 것 같습니다.

---
+++ app/src/main/java/com/example/myapplication/Common/ShareInterface.java
@@ -0,0 +1,12 @@
+package com.example.myapplication.Common;
+
+import com.example.myapplication.CallBackend.CallListener;
+
+/**
+ * @author Yoon
+ * @created 2021-09-13
+ */
+public class ShareInterface {
+
+    public static CallListener callListener = null;
+}
